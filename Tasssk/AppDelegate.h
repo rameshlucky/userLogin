9ed08bd6cdd462890248071f74a21db656780579
fis <fis@fiss-mac-3.local>
2016-05-26 18:49:11 +0530

    Initial Commit

---
+++ Tasssk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tasssk
+//
+//  Created by fis on 5/26/16.
+//  Copyright © 2016 fis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tasssk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tasssk
+//
+//  Created by fis on 5/26/16.
+//  Copyright © 2016 fis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
